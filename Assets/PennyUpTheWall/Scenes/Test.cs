9f106d67e2e4b1bfb5d98343389f9ea357b7d559
edamderbel <96633485+edamderbel@users.noreply.github.com>
2023-01-02 22:49:23 +0100

    AWS

---
+++ Assets/PennyUpTheWall/Scenes/Test.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Amazon;
+using Amazon.S3;
+using Amazon.S3.Model;
+using System.Text.RegularExpressions;
+
+public class Test : MonoBehaviour
+{
+    AmazonS3Client s3Client;
+
+    private void Start()
+    {
+        UnityInitializer.AttachToGameObject(gameObject);
+        AWSConfigs.HttpClient = AWSConfigs.HttpClientOption.UnityWebRequest;
+        s3Client = new AmazonS3Client(Credentials.UID, Credentials.Secret, RegionEndpoint.EUWest1);
+
+        StartDownloading();
+    }
+
+#if UNITY_ANDROID
+    public void UsedOnlyForAOTCodeGeneration()
+    {
+        //Bug reported on github https://github.com/aws/aws-sdk-net/issues/477
+        //IL2CPP restrictions: https://docs.unity3d.com/Manual/ScriptingRestrictions.html
+        //Inspired workaround: https://docs.unity3d.com/ScriptReference/AndroidJavaObject.Get.html
+
+        AndroidJavaObject jo = new AndroidJavaObject("android.os.Message");
+        int valueInt = jo.Get<int>("what");
+        string valueString = jo.Get<string>("what");
+    }
+#endif
+
+    public void StartDownloading()
+    {
+        Debug.Log("The Downloading is Start");
+    }
+}
---
+++ Assets/PennyUpTheWall/Scripts/Credentials.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Credentials
+{
+    public static string UID = "AKIAXWVDESS775WXRDY4";
+    public static string Secret = "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";
+    public static string bucket = "pennyupthewall";
+}
